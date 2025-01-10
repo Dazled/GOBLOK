c19fa9c174c6f0bc07f813a98ad53652840121fc
Your NameDazled <danielw0703@gmail.com>
2025-01-10 16:10:52 +0700

    Menambahkan file node modules

---
+++ script.js
@@ -0,0 +1,40 @@
+console.log("Hello World")
+
+let x = 1;
+x=3;
+
+const y=2;
+
+if (x >y){
+    console.log("x > y")
+}
+else{
+    console.log("x <= y")
+}
+
+for( let i= 0; i<= 10; i+=1){
+    console.log(i)
+}
+
+const array=["1",2,true, "test"]
+
+console.log(array[0])
+
+const obj = {
+    "nilai": 80,
+    "nama": "Ivan"
+}
+
+console.log(obj),
+console.log(obj.nilai)
+ // JSON= Javascript Object Notation
+const arrayObj=[{
+    "nilai":80,
+    "nama":"Ivan"
+},
+{
+    "nilai":75,
+    "nama" : "Dedi"
+}]
+console.log(arrayObj),
+console.log(arrayObj[1].nama)
